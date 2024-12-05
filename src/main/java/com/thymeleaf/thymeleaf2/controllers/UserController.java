4f85221d579e252654f07d3fc09b909a8c856698
Pricecito <matas.gutierrez.1254@gmail.com>
2024-12-04 20:27:22 -0500

    iteraciones

--- src/main/java/com/thymeleaf/thymeleaf2/controllers/UserController.java
+++ src/main/java/com/thymeleaf/thymeleaf2/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.thymeleaf.thymeleaf2.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +29,21 @@ public class UserController {
     public String linkExpression(Model model) {
         model.addAttribute("id", 1);
         return "link/index";
+    }
+
+    @GetMapping("fragment-expression")
+    public String fragmentExpression() {
+        return "fragment/index";
+    }
+
+    @GetMapping("iterations")
+    public String interations(Model modelo) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Ramesh", "ramesh@gmail.com", "125"));
+        users.add(new User(2, "John", "john@gmail.com", "123"));
+        users.add(new User(3, "Mike", "mike@gmail.com", "456"));
+        modelo.addAttribute("users", users);
+        return "iteration/index";
     }
 
 }
